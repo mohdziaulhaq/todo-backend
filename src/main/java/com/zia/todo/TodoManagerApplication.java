d9828df0d54e432fdd4b87914765abd45e314654
Mohd Zia Ul Haq <mohdziaulhaq123@gmail.com>
2024-02-20 16:28:23 -0600

    first commit

---
+++ src/main/java/com/zia/todo/TodoManagerApplication.java
@@ -0,0 +1,15 @@
+package com.zia.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagerApplication.class, args);
+
+		System.out.println("Hyd");
+	}
+
+}
